726ee7cf258a59787d59f9c18e69bc22ee75a47e
Darshan Baral <darshanbaral@gmail.com>
2021-09-12 14:54:57 -0700

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(name="pdrle",
-      version="0.3",
+      version="0.4",
       description="python package for run length encoding on pandas Series",
       url="https://github.com/darshanbaral/pyrle",
       author="Darshan Baral",
