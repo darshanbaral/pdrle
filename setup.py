ef45f4f91f7babe4437d951824cb8777dd8d95ab
Darshan Baral <darshanbaral@gmail.com>
2021-09-11 22:08:59 -0700

    added metadata

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ setup(name="pdrle",
       description="python package for run length encoding on pandas Series",
       url="https://github.com/darshanbaral/pyrle",
       author="Darshan Baral",
+      author_email="darshanbaral@gmail.com",
       license="MIT",
       packages=["pdrle"],
+      download_url="https://github.com/darshanbaral/pdrle/archive/refs/tags/0.3.tar.gz",
       install_requires=["pandas"]
       )
